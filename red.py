a79cf84c08c8b702399e681b57bc413d2cce13c1
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-17 10:04:27 -0400

    Update red.py

--- red.py
+++ red.py
@@ -16,3 +16,84 @@ def draw():
   global stars, current_level, game_over, game_complete
   screen.clear()
   screen.blit("space", (0:0))
+  if game_over:
+    display_message("GAME OVER!", "Try Again.")
+  elif game_complete:
+    display_message("YOU WON!", "WELL DONE")
+  else:
+    for star in stars:
+      star.draw()
+def update():
+  global stars, game_complete, game_over, current_level
+  if len(stars) == 0:
+    stars = make_stars(current_level)
+  if (game_complete  game_over) and keyboard.space:
+    stars= []
+    current_level = 1
+    game_complete = False
+    game_over = False
+def make_stars(number_of_extra_stars):
+  colors_to_create = get_colors_to_create(to_create)
+  layout_stars(new_stars)
+  animate_stars(new_stars)
+  return new_stars
+def get_colors_to_create(number_of_extra_stars):
+  colors_to_create = ["red"]
+  for i in range(0, number_of_extra_stars):
+    random_color = random.choice(COLORS)
+    colors_to_create.append(random_color)
+    return colors_to_create
+def create_stars(colors_to_create):
+  new_stars = []
+  for color in colors_to_create:
+    star = Actor(color + "-star")
+    return new star
+  
+def layout_stars(stars_to_layout):
+  number_of_gaps = len(stars_to_layout) + 1
+  gap_size = WIDTH / number_of_gaps
+  random.shuffle(stars_to_layout)
+  for index, star in enumerate(stars_to_layout):
+    new_x_pos = (index + 1) - gap_size
+    star.x = new_x_pos
+    if index % 2 == 0:
+      star.y = 0
+     else:
+      star.y = HEIGHT
+def animate_stars(stars_to_animate):
+  for star in stars_to_animate:
+    random_speed_adjustment = random.randint(0.2)
+    duration = START_SPEED - current_level
+    star.anchor = ("center", "bottom")
+    animation = animate(star, duration=duration, on_finished=handle_game_over, y=HEIGHT)
+    animations.append(animation)
+def handle_game_over():
+  global game_over
+  game_over = True
+def on_mouse_down(pos):
+  global stars, current_level
+  for star in stars:
+    if star.collidepoint(pos):
+      if "red" in star.image:
+        red_star_click()
+      else:
+        handle_game_over()
+def stop_animations(animations_to_stop):
+  for animation in animations_to_stop:
+    if animation.running:
+      animation.stop()
+def display_message(heading_text, sub_heading_text):
+  screen.draw.text(heading_text, fontsize=60, center=CENTER, color=FONT_COLOR)
+  screen.draw.text(sub_heading_text,
+                   fontsise=30,
+                   center=(CENTER_X, CENTER_Y + 30),
+                   color=FONT_COLOR)  
+def shuffle():
+  global stars
+  if stars:
+    x_values = [star.x for star in stars]
+    random.shuffle(x_values)
+    for index, star in enumerate(stars):
+      new_x = x_values[index]
+      animation = animste(star, duration=0.5, x=new_x)
+clock.schedule_interval(shuffle, 1)
